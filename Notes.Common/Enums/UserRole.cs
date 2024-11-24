f54fdfb068e5c52a82d42dbebdc86ccfa4b31777
sanidhyapandey <pandeysanid@gmail.com>
2024-11-24 21:35:11 +0000

    Added Enums for User

---
+++ Notes.Common/Enums/UserRole.cs
@@ -0,0 +1,7 @@
+namespace Notes.Common.Enums;
+
+public enum UserRole
+{
+    User = 1,
+    Admin = 2,
+}
---
+++ Notes.Common/Enums/UserStatus.cs
@@ -0,0 +1,8 @@
+namespace Notes.Common.Enums;
+
+public enum UserStatus
+{
+Active = 1,
+Inactive = 2,
+    
+}
