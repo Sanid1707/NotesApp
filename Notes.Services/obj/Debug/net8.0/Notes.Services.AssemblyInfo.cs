a820e7c3f675ce19198193be5bdaa6cb8bf51b84
v-philip <102297752+v-philip@users.noreply.github.com>
2024-12-01 00:40:34 +0000

    worked on corrected note title api and worked on skelton for notedahboard

--- Notes.Services/obj/Debug/net8.0/Notes.Services.AssemblyInfo.cs
+++ Notes.Services/obj/Debug/net8.0/Notes.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd8a1ddf3e35f2a31c560ebb3f97cd45a15c041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea839a2a6dd6f9cdc9d48a0c164c74b62667554")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- notes-react/src/Pages/dashboard/NotesDash.js
+++ notes-react/src/Pages/dashboard/NotesDash.js
@@ -2,9 +2,56 @@
 
 
 const NotesDash = () => {
+
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // make a an API call to get notes
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const response = await fetch('http://localhost:5189/api/notes', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status} ${response.statusText}`);
+                }
+
+                const result = await response.json();
+                setNotes(result);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchNotes();
+    }, []);
+    
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    // if notes is empty
+    if (notes.length === 0) {
+        return( 
+        <div>add note</div>);
+    }
+
+    
+
+
     return (
         <div>
             <h1>Notes Dashboard</h1>
         </div>
+
     );
 }
+
+export default NotesDash;
